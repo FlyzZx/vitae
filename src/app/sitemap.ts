ca62eb841fe05cc212fe9adad21f54bfd77210b5
Nicolas Faraci <FlyzZx@users.noreply.github.com>
2025-10-09 21:38:48 +0200

    Update site url (#3)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nicolas-faraci.dev' // À remplacer par votre domaine
+  const baseUrl = 'https://www.nicolasfaraci.fr' // À remplacer par votre domaine
 
   return [
     {
